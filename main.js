98e842d7d2c7fd62e096e12508cc6cc6fa6b4e07
codingwolf-at <atameshwari9@gmail.com>
2021-01-01 23:26:01 +0530

    logic working

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-// ref to name input
-const nameInput = document.querySelector('#name');
-// ref to msg heading
-const msgOutput = document.querySelector('#msg');
+// ref to DOB input 
+const dobInput = document.querySelector('#date');
+// ref to lucky number
+const numberInput = document.querySelector('#number');
+// ref to check button
+const checkButton = document.querySelector('#button');
 
-// to read name and to greet user
-nameInput.addEventListener("input", function(e)) {
-    const message = `Welcome ${e.target.value.toUpperCase()} !
-    Let's see some facts about your birthday!`;
-    msgOutput = innerHTML = message;
+// declaring function amILucky
+function amILucky() {
+    const dateOfBirth = (dobInput.value).split("-");
+    const sumOfDOB = add(dateOfBirth);
+    const luckyNumber = numberInput.value;
+    const luck = checkLuck(sumOfDOB, luckyNumber);
+    console.log(luck)
+}
+
+// adding event listener to check button
+checkButton.addEventListener("click", amILucky);
+
+// FUNCTIONS
+
+// function to return sum of digits of DOB
+function add(dateOfBirth) {
+    let sum = 0;
+    dateOfBirth.map(value => {
+        sum = sum + parseInt(value);
+    })
+    return sum;
+}
+
+// function to check birthday for luck
+function checkLuck(sum, num) {
+    if(sum%num === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
